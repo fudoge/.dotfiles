b7291ff4187d4a72afc1929d9ac959320d4744d9
fudoge <77766067+fudoge@users.noreply.github.com>
2025-09-21 22:28:35 +0900

    Changed old lspconfig to modern one

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,32 @@
 local keyMapper = require("utils.keyMapper").mapKey
 
+local function go_root(fname)
+    return vim.fs.root(fname or 0, { "go.work", "go.mod", ".git" })
+end
+
+local lsp_list = {
+    "lua_ls",
+    "ts_ls",
+    "gopls",
+    "bashls",
+    "cssls",
+    "clangd",
+    "docker_compose_language_service",
+    "graphql",
+    "jdtls",
+    "biome",
+    "ltex",
+    "nginx_language_server",
+    "sqls",
+    "yamlls",
+    "pyright",
+    "dockerls",
+    "terraformls",
+    "helm_ls",
+    "gh_actions_ls"
+
+}
+
 return {
     {
         "williamboman/mason.nvim",
@@ -9,28 +36,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         dependencies = { "neovim/nvim-lspconfig" },
         opts = {
-            ensure_installed = {
-                "lua_ls",
-                "ts_ls",
-                "gopls",
-                "bashls",
-                "cssls",
-                "clangd",
-                "docker_compose_language_service",
-                "graphql",
-                "jdtls",
-                "biome",
-                "ltex",
-                "nginx_language_server",
-                "sqls",
-                "yamlls",
-                "pyright",
-                "dockerls",
-                "terraformls",
-                "helm_ls",
-                "gh_actions_ls"
-            },
-            automatic_installation = true,
+            ensure_installed = lsp_list, automatic_installation = true,
         },
         config = function()
             require("mason").setup({})
@@ -42,11 +48,14 @@ return {
         config = function()
             vim.lsp.config("lua_ls", {})
             vim.lsp.config("ts_ls", {})
-            vim.lsp.config("gopls", {})
+            vim.lsp.config("gopls", {
+                root_dir = go_root,
+                single_file_support = false,
+            })
             vim.lsp.config("bashls", {})
             vim.lsp.config("cssls", {})
             vim.lsp.config("clangd", {
-                cmd = { "clangd", "--header-insertion=never", "--query-driver=/usr/bin/g++", "--fallback-style=Google", "--include-directory=/usr/local/include" },
+                cmd = { "clangd", "--header-insertion=never", "--query-driver=/usr/bin/g++", "--fallback-style=Google" },
             })
             vim.lsp.config("docker_compose_language_service", {})
             vim.lsp.config("graphql", {})
@@ -62,27 +71,13 @@ return {
             vim.lsp.config("helm_ls", {})
             vim.lsp.config("gh_actions_ls", {})
 
-            vim.lsp.enable({
-                "lua_ls",
-                "ts_ls",
-                "gopls",
-                "bashls",
-                "cssls",
-                "clangd",
-                "docker_compose_language_service",
-                "graphql",
-                "jdtls",
-                "biome",
-                "ltex",
-                "nginx_language_server",
-                "sqls",
-                "yamlls",
-                "pyright",
-                "dockerls",
-                "terraformls",
-                "helm_ls",
-                "gh_actions_ls"
-            })
+            local need = {}
+            for _, name in ipairs(lsp_list) do
+                if #vim.lsp.get_clients({ name = name }) == 0 then
+                    table.insert(need, name)
+                end
+            end
+            if #need > 0 then vim.lsp.enable(need) end
 
             -- vim.lsp.buf.hover
             -- vim.lsp.buf.definition
