cb229982735ae63edd1e610ab16e807c6defbadd
fudoge <77766067+fudoge@users.noreply.github.com>
2025-09-21 22:55:08 +0900

    Changed old lspconfig to modern one

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,35 @@
 local keyMapper = require("utils.keyMapper").mapKey
 
-local function go_root(fname)
-    return vim.fs.root(fname or 0, { "go.work", "go.mod", ".git" })
+local function root(patterns, fname)
+    return vim.fs.root(fname or 0, patterns)
+end
+
+local roots = {
+    lua_ls = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", "stylua.toml", "selene.toml", "seleme.yml", ".git" },
+    ts_ls = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+    gopls = { "go.work", "go.mod", ".git" },
+    pyright = { "pyproject.toml", "setup.py", "setup.cfg", "requrements.txt", "Pipfile", "pyrightconfig.json", ".git" },
+    yamlls = { ".git" },
+    terraformls = { ".terraform", ".git" },
+    dockerls = { "Dockerfile", ".git" },
+    cssls = { "package.json", ".git" },
+    bashls = { ".git" },
+    graphql = { ".git" },
+    sqls = { ".config.yml", ".git" },
+    clangd = { ".clangd", "compile_commands.json", "compile_flags.json", ".git" },
+    docker_compose_language_service = { "docker-compose.yaml", "docker-compose.yml", "compose.yml", "compose.yaml", ".git" },
+    ltex = { ".git" },
+    nginx_language_server = { "nginx.conf", ".git" },
+    jdtls = { ".git", "pom.xml", "build.gradle", "build.gradle.kts" },
+    biome = { "biome.json", "biome.jsonc", "package.json", ".git" },
+    helm_ls = { "Chart.yaml", ".git" },
+    gh_actions_ls = { ".git" }
+}
+
+local function rd(name)
+    return function(fname)
+        return root(roots[name] or { ".git" }, fname)
+    end
 end
 
 local lsp_list = {
@@ -46,44 +74,47 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.config("lua_ls", {})
-            vim.lsp.config("ts_ls", {})
-            vim.lsp.config("gopls", {
-                root_dir = go_root,
-                single_file_support = false,
-            })
-            vim.lsp.config("bashls", {})
-            vim.lsp.config("cssls", {})
-            vim.lsp.config("clangd", {
-                cmd = { "clangd", "--header-insertion=never", "--query-driver=/usr/bin/g++", "--fallback-style=Google" },
-            })
-            vim.lsp.config("docker_compose_language_service", {})
-            vim.lsp.config("graphql", {})
-            vim.lsp.config("jdtls", {})
-            vim.lsp.config("biome", {})
-            vim.lsp.config("ltex", {})
-            vim.lsp.config("nginx_language_server", {})
-            vim.lsp.config("sqls", {})
-            vim.lsp.config("yamlls", {})
-            vim.lsp.config("pyright", {})
-            vim.lsp.config("dockerls", {})
-            vim.lsp.config("terraformls", {})
-            vim.lsp.config("helm_ls", {})
-            vim.lsp.config("gh_actions_ls", {})
+            vim.lsp.config("*", { single_file_support = false })
+            vim.lsp.config("lua_ls", { root_dir = rd("lua_ls") })
+            vim.lsp.config("ts_ls", { root_dir = rd("ts_ls") })
+            vim.lsp.config("gopls", { root_dir = rd("gopls") })
+            vim.lsp.config("bashls", { root_dir = rd("bashls") })
+            vim.lsp.config("cssls", { root_dir = rd("cssls") })
+            vim.lsp.config("clangd", { root_dir = rd("cssls") },
+                { cmd = { "clangd", "--header-insertion=never", "--query-driver=/usr/bin/g++", "--fallback-style=Google" } })
+            vim.lsp.config("docker_compose_language_service", { root_dir = rd("docker_compose_language_service") })
+            vim.lsp.config("graphql", { root_dir = rd("graphql") })
+            vim.lsp.config("jdtls", { root_dir = rd("jdtls") })
+            vim.lsp.config("biome", { root_dir = rd("biome") })
+            vim.lsp.config("ltex", { root_dir = rd("ltex") })
+            vim.lsp.config("nginx_language_server", { root_dir = rd("nginx_language_server") })
+            vim.lsp.config("sqls", { root_dir = rd("sqls") })
+            vim.lsp.config("yamlls", { root_dir = rd("yamlls") })
+            vim.lsp.config("pyright", { root_dir = rd("pyright") })
+            vim.lsp.config("dockerls", { root_dir = rd("dockerls") })
+            vim.lsp.config("terraformls", { root_dir = rd("terraformls") })
+            vim.lsp.config("helm_ls", { root_dir = rd("helm_ls") })
+            vim.lsp.config("gh_actions_ls", { root_dir = rd("gh_actions_ls") })
 
-            local need = {}
-            for _, name in ipairs(lsp_list) do
-                if #vim.lsp.get_clients({ name = name }) == 0 then
-                    table.insert(need, name)
+            local function enable_for_buf(names)
+                local bufnr = 0
+                local need = {}
+                for _, name in ipairs(names) do
+                    if #vim.lsp.get_clients({ name = name, bufnr = bufnr }) == 0 then
+                        table.insert(need, name)
+                    end
                 end
+                if #need > 0 then vim.lsp.enable(need) end
             end
-            if #need > 0 then vim.lsp.enable(need) end
 
-            -- vim.lsp.buf.hover
+            enable_for_buf(lsp_list)
+            vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+                callback = function() enable_for_buf(lsp_list) end
+            })
+
             -- vim.lsp.buf.definition
             -- vim.lsp.buf.code_action
-            keyMapper("K", vim.lsp.buf.hover)
-            keyMapper("gd", vim.lsp.buf.definition)
+            keyMapper("gd", vim.lsp.buf.definition())
             keyMapper("<leader>ca", vim.lsp.buf.code_action)
         end,
     },
